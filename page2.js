b18e5a7eb35709fb9f6cf16de6c4d2516b2b1776
GeneralWolf <49901138+GeneralWolf@users.noreply.github.com>
2020-03-18 23:18:28 +0000

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+const opacDisabled = 0.3;  //transparência para botões desactivados
+const imgFolder = "../resources/image/";
+const txtFolder = "../resources/text/";
+const audioVolume = 1;
+*/
+
+(function()
+{
+	window.addEventListener("load", main);
+}());
+
+
+function main()
+{
+	/*
+	ADD CODE
+	*/
+}
+
+
+/*
+ADD CODE
+*/
